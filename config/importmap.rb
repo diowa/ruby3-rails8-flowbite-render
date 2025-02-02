7230299269006471f547aaf9afac2e68f03e23f3
Geremia Taglialatela <tagliala.dev@gmail.com>
2025-02-02 16:05:06 +0100

    Add Flowbite Turbo
    ```
    bin/importmap pin flowbite/dist/flowbite.turbo.min.js --from=jsdelivr
    ```

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,4 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "flowbite/dist/flowbite.turbo.min.js", to: "flowbite--dist--flowbite.turbo.min.js.js" # @3.1.1
