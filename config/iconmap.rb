57430438c166b173e74cdca5c175e6ac17e20fd5
Geremia Taglialatela <tagliala.dev@gmail.com>
2025-07-23 09:33:43 +0200

    Update icons
    ```
    bin/iconmap update
    ```

--- config/iconmap.rb
+++ config/iconmap.rb
@@ -1,4 +1,3 @@
 # Pin icons by running ./bin/iconmap
 
-pin "@fortawesome/fontawesome-free/svgs/brands/github.svg", to: "@fortawesome--fontawesome-free--svgs--brands--github.svg" # @6.0.0
-pin "@fortawesome/fontawesome-free/svgs/brands/instagram.svg", to: "@fortawesome--fontawesome-free--svgs--brands--instagram.svg" # @6.0.0
+pin "@fortawesome/fontawesome-free/svgs/brands/github.svg", to: "@fortawesome--fontawesome-free--svgs--brands--github.svg" # @7.0.0
